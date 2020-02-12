838a453b5ae041e7fc8e6499a859f0633f709e81
Jimmy Ma <a468963@gmail.com>
2020-02-13 00:35:08 +0800

    move TodoList code to class

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Todo from './Todo'
 import { VisibilityFilters } from '../actions'
@@ -6,24 +6,55 @@ import { VisibilityFilters } from '../actions'
 // TodoList 是一個顯示可見 todos 的清單。
 // todos: Array 是一個有著 { id, text, completed } 形狀的 todo 項目的陣列
 
-const TodoList = ({ todos, filter }) => {
-  if (filter.FILTER_CODE === VisibilityFilters.SHOW_FINISH) {
-    todos = todos.filter(t => { return t.status.code == 3 || t.status.code == 4 })
-  } else if (filter.FILTER_CODE === VisibilityFilters.SHOW_TODO) {
-    todos = todos.filter(t => { return t.status.code == 1 || t.status.code == 2 })
+export default class TodoList extends Component {
+  constructor({ todos, filter }) {
+    super();
+    this.state = {
+      todos: todos,
+      filter: filter,
+    }
   }
-  return (
-    <div className="container">
-      <div className="Status">
-        <div className="title">
-          <div className="bar"></div>
-          <h1>{filter.STATUS}</h1>
+  componentDidMount() {
+    if (this.state.filter.FILTER_CODE === VisibilityFilters.SHOW_FINISH) {
+      this.state.todos = this.state.todos.filter(t => { return t.status.code == 3 || t.status.code == 4 })
+    } else if (this.state.filter.FILTER_CODE === VisibilityFilters.SHOW_TODO) {
+      this.state.todos = this.state.todos.filter(t => { return t.status.code == 1 || t.status.code == 2 })
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="Status">
+          <div className="title">
+            <div className="bar"></div>
+            <h1>{this.state.filter.STATUS}</h1>
+          </div>
+          {this.state.todos.map(todo => <Todo key={todo.id} {...todo} />)}
         </div>
-        {todos.map(todo => <Todo key={todo.id} {...todo} />)}
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+// const TodoList = ({ todos, filter }) => {
+//   if (filter.FILTER_CODE === VisibilityFilters.SHOW_FINISH) {
+//     todos = todos.filter(t => { return t.status.code == 3 || t.status.code == 4 })
+//   } else if (filter.FILTER_CODE === VisibilityFilters.SHOW_TODO) {
+//     todos = todos.filter(t => { return t.status.code == 1 || t.status.code == 2 })
+//   }
+//   return (
+//     <div className="container">
+//       <div className="Status">
+//         <div className="title">
+//           <div className="bar"></div>
+//           <h1>{filter.STATUS}</h1>
+//         </div>
+//         {todos.map(todo => <Todo key={todo.id} {...todo} />)}
+//       </div>
+//     </div>
+//   )
+// }
 
 // 宣告TodoList的參數型態
 // TodoList.propTypes = {
@@ -34,4 +65,4 @@ const TodoList = ({ todos, filter }) => {
 //   }).isRequired).isRequired,
 // }
 
-export default TodoList
+// export default TodoList
