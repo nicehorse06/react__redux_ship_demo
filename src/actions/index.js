c5b8c8bd2dd7ff8c4b6d0b0e6591d394522bda6b
Jimmy Ma <a468963@gmail.com>
2020-02-13 23:50:53 +0800

    移除舊專案代碼

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,4 @@
-let nextTodoId = 0
 let nextShipId = 0
-
-// 用function 回傳 action 為 action creator
-// 實際被使用的狀況為 dispatch(addTodo(text))
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
-})
 
 export const addShipList = ship_data => ({
   type: 'ADD_SHIP_LIST',
@@ -16,11 +7,6 @@ export const addShipList = ship_data => ({
   logo: ship_data.logo,
   status: ship_data.status,
   date: ship_data.date,
-})
-
-export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
 })
 
 export const VisibilityFilters = {
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Header from './Header'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import { SHIP_FILTER, addShipList } from '../actions'
 import { store } from '../index'
